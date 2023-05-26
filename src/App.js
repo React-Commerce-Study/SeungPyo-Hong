08ba1bbddbd298bf97a25b05b6bf395a4a67c98a
SeungPyo Hong <83650872+Jaharim@users.noreply.github.com>
2023-05-26 23:08:40 +0900

    ✨ feat: App.js에 Components import (#4)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
+import Header from './components/header/Header';
+import Slider from './components/slider/Slider';
+import Main from './components/main/Main';
+import Footer from './components/footer/Footer';
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
+
+  .a11y-hidden {
+    clip: rect(1px, 1px, 1px, 1px);
+    clip-path: inset(50%);
+    width: 1px;
+    height: 1px;
+    margin: -1px;
+    overflow: hidden;
+    padding: 0;
+    position: absolute;
+  }
 `;
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <div>hello world</div>
+      <Header />
+      <Slider />
+      <Main />
+      <Footer />
     </>
   );
 }
