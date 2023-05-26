cf5c11e4eb782852982048b5464b375cc47ee5a5
SeungPyo Hong <83650872+Jaharim@users.noreply.github.com>
2023-05-26 23:07:58 +0900

    ✨ feat: Footer 컴포넌트 생성 (#4)

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,117 @@
+import React from 'react';
+import styled from 'styled-components';
+import instaIcon from '../../assets/images/icon-insta.svg';
+import fbIcon from '../../assets/images/icon-fb.svg';
+import youtubeIcon from '../../assets/images/icon-yt.svg';
+
+export default function Footer() {
+  const FooterStyle = styled.footer`
+    display: flex;
+    justify-content: center;
+    max-width: 100vw;
+    margin: 0 auto;
+    margin-top: 230px;
+    background-color: #f2f2f2;
+
+    .wrapper {
+      display: flex;
+      flex-direction: column;
+      max-width: 1280px;
+      box-sizing: border-box;
+      height: 300px;
+      padding: 60px 20px 0 20px;
+      font-size: 14px;
+    }
+
+    .footer-nav {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+
+    .footer-nav-site {
+      a {
+        text-decoration: none;
+      }
+
+      a:visited {
+        color: black;
+      }
+
+      a strong {
+        font-weight: bold;
+      }
+      span {
+        margin: 0 14px;
+      }
+    }
+
+    .footer-nav-sns {
+      display: flex;
+      gap: 14px;
+    }
+
+    .footer-horizon-line {
+      display: block;
+      width: 1280px;
+      height: 1px;
+      background-color: #c4c4c4;
+      margin-top: 22px;
+      margin-bottom: 30px;
+    }
+
+    .footer-company-desc {
+      display: flex;
+      flex-direction: column;
+      gap: 10px;
+      color: #767676;
+
+      span strong {
+        font-weight: bold;
+      }
+    }
+  `;
+  return (
+    <FooterStyle>
+      <section className='wrapper'>
+        <div className='footer-nav'>
+          <div className='footer-nav-site'>
+            <a href='#'>호두샵소개</a>
+            <span>|</span>
+            <a href='#'>이용약관</a>
+            <span>|</span>
+            <a href='#'>
+              <strong>개인정보처리방침</strong>
+            </a>
+            <span>|</span>
+            <a href='#'>전자금융거래약관</a>
+            <span>|</span>
+            <a href='#'>청소년보호정책</a>
+            <span>|</span>
+            <a href='#'>제휴문의</a>
+          </div>
+          <div className='footer-nav-sns'>
+            <a href='#'>
+              <img src={instaIcon} alt='인스타그램링크' />
+            </a>
+            <a href='#'>
+              <img src={fbIcon} alt='페이스북링크' />
+            </a>
+            <a href='#'>
+              <img src={youtubeIcon} alt='유튜브링크' />
+            </a>
+          </div>
+        </div>
+        <span className='footer-horizon-line'></span>
+        <div className='footer-company-desc'>
+          <span>
+            <strong>(주)HODU SHOP</strong>
+          </span>
+          <span>제주특별자치도 제주시 동광고 137 제주코딩베이스캠프</span>
+          <span>사업자 번호 : 000-0000-0000 | 통신판매업</span>
+          <span>대표 : 김호두</span>
+        </div>
+      </section>
+    </FooterStyle>
+  );
+}
